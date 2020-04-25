d733c7bfb2fb60cc719f97d21a7a65de8195b534
hriteek <hriteekbista@gmail.com>
2020-04-25 21:46:50 +0545

    write to a file

---
+++ asynchronous.write.js
@@ -0,0 +1,9 @@
+const { writeFile } = require('fs');
+
+writeFile(
+  './data/app/log',
+  '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /dot-com" 405 21512',
+  (err) => {
+    err ? console.error(err) : console.log('Filed modified');
+  }
+);
