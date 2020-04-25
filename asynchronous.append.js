8d70019a6b19c912d9373013eff4afa0f650d834
hriteek <hriteekbista@gmail.com>
2020-04-25 21:50:05 +0545

    working appending to file

--- asynchronous.append.js
+++ asynchronous.append.js
@@ -1,10 +1,9 @@
-const { writeFile } = require('fs');
+const { appendFile } = require('fs');
 
-writeFile(
-  './data/app/log',
+appendFile(
+  './data/app.log',
   '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /dot-com" 405 21512',
-  { flag: 'a' },
   (err) => {
-    err ? console.error(err) : console.log('Filed modified');
+    err ? console.error(err) : console.log('File modified');
   }
 );
