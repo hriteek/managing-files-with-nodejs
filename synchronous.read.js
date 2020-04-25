2fd949fd3ec744f0bad2ec4e873e5e1ddcaa7582
hriteek <hriteekbista@gmail.com>
2020-04-25 18:11:47 +0545

    synchronous file read

---
+++ synchronous.read.js
@@ -0,0 +1,10 @@
+const { convertCsv } = require('./csv.parse');
+const { readFileSync } = require('fs');
+
+// this is synchronous code to read from the file using callback
+try {
+  const data = readFileSync('./data/pulitzer-circulation-data.csv', 'utf8');
+  console.table(convertCsv(data));
+} catch (error) {
+  console.error(error);
+}
