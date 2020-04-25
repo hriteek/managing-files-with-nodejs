7d2c2295a46e79a09d8b0664c51bc9131a6daec4
hriteek <hriteekbista@gmail.com>
2020-04-25 18:47:33 +0545

    update

--- file.descriptor.error.js
+++ file.descriptor.error.js
@@ -1,6 +1,11 @@
-const fs = require("fs");
+const fs = require('fs');
 
 console.log('Opening files...');
 for (let i = 0; i < 50000; i++) {
-  const fd = fs.openSync("./data/app.log");
+  const fd = fs.openSync('./data/app.log');
+  console.log(fd);
+  // fs.closeSync(fd);
+  fs.close(fd, () => {});
 }
+
+// > anytime you have a file descriptor, you are responsible for closing the file
