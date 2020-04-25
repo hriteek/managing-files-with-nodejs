bd83398c305a8b161aeecd8212040bfa057cb285
hriteek <hriteekbista@gmail.com>
2020-04-25 18:05:42 +0545

    asynchrnously read a file

---
+++ asynchronous.read.js
@@ -0,0 +1,12 @@
+const { convertCsv } = require('./csv.parse');
+const { readFile } = require('fs');
+
+// this is asynchronous code to read from the file using callback
+readFile('./data/pulitzer-circulation-data.csv', 'utf8', (error, data) => {
+  if (error) {
+    console.log({ error });
+    return;
+  }
+  const val = convertCsv(data);
+  console.table(val);
+});
